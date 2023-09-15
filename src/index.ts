e6358747c52d60fea471d247f27ee1124b78e2ae
Frank Schönhofer <frankschoenhofer@gmail.com>
2023-09-15 11:16:16 +0200

    ambient occlusion

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,33 @@ import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader';
 import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader";
 import {KTX2Loader} from "three/examples/jsm/loaders/KTX2Loader";
 import {REVISION} from "three";
+import {EffectComposer} from "three/examples/jsm/postprocessing/EffectComposer";
+import {RenderPass} from "three/examples/jsm/postprocessing/RenderPass";
+import {SAOPass} from "three/examples/jsm/postprocessing/SAOPass";
+import {OutputPass} from "three/examples/jsm/postprocessing/OutputPass";
+import GUI from "three/examples/jsm/libs/lil-gui.module.min";
+import Stats from "three/examples/jsm/libs/stats.module";
 
 let camera: THREE.PerspectiveCamera,
     scene: THREE.Scene,
-    renderer: THREE.WebGLRenderer;
+    controls: OrbitControls,
+    renderer: THREE.WebGLRenderer,
+    composer: EffectComposer,
+    stats: Stats,
+    model: THREE.Group,
+    saoPass: SAOPass,
+    isDragging: boolean = false;
 
 function init(): void {
-// setup //
+    // setup //
+    const container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
     renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.autoClear = true;
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+
+    container.appendChild(renderer.domElement);
 
     scene = new THREE.Scene();
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -32,30 +49,40 @@ function init(): void {
     camera.position.set(-1.8, 1.5, 2.7);
     camera.clear()
 
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.addEventListener('change', render); // use if there is no animation loop
+    /// post processsing ///
+    composer = new EffectComposer( renderer );
+    const renderPass = new RenderPass( scene, camera );
+    composer.addPass( renderPass );
+    saoPass = new SAOPass( scene, camera );
+    saoPass.params.saoIntensity = 0.1;
+    composer.addPass( saoPass );
+    const outputPass = new OutputPass();
+    composer.addPass( outputPass );
+
+    /// controls ///
+    controls = new OrbitControls(camera, renderer.domElement);
+    // controls.addEventListener('change', render); // use if there is no animation loop
+    controls.addEventListener('start', () => isDragging = true);
+    controls.addEventListener('end', () => isDragging = false);
     controls.minDistance = 0.5;
     controls.maxDistance = 10;
     controls.target.set(0, 0.6, 0);
-    controls.update();
     controls.update();
     renderer.render(scene, camera);
 
     scene.background = new THREE.Color(0x444444);
     // skybox
-/*
-        new RGBELoader()
-            .setPath('assets/')
+    new RGBELoader()
+        .setPath('assets/')
 
-            .load('venice_sunset_1k.hdr', (texture) => {
-                texture.mapping = THREE.EquirectangularReflectionMapping;
+        .load('venice_sunset_1k.hdr', (texture) => {
+            texture.mapping = THREE.EquirectangularReflectionMapping;
 
-                // scene.background = texture;
-                scene.environment = texture;
+            // scene.background = texture;
+            scene.environment = texture;
 
-                render();
-            });
-*/
+            render();
+        });
 
     // model
     const loader = new GLTFLoader().setPath('assets/');
@@ -71,6 +98,7 @@ function init(): void {
     loader.setKTX2Loader(ktx2Loader.detectSupport(renderer))
 
     loader.load('KTM_SX_250_2023.glb', function (gltf) {
+        model = gltf.scene as THREE.Group;
         scene.add(gltf.scene);
 
         render();
@@ -79,13 +107,38 @@ function init(): void {
 
     /// lighting ///
 
-        const light = new THREE.AmbientLight(0xffffff);
-        light.position.set(10, 10, 10);
-        scene.add(light);
+    const light = new THREE.AmbientLight(0xffffff);
+    light.position.set(10, 10, 10);
+    scene.add(light);
 
-        const sun = new THREE.DirectionalLight(0xffffff);
-        sun.position.set(10, 10, 10);
-        scene.add(sun);
+    const sun = new THREE.DirectionalLight(0xffffff);
+    sun.position.set(10, 10, 10);
+    scene.add(sun);
+
+    // Init gui
+    stats = new Stats();
+    container.appendChild( stats.dom );
+
+/*    const gui = new GUI();
+    gui.add( saoPass.params, 'output', {
+        'Default': SAOPass.OUTPUT.Default,
+        'SAO Only': SAOPass.OUTPUT.SAO,
+        'Normal': SAOPass.OUTPUT.Normal
+    } ).onChange( function ( value ) {
+
+        saoPass.params.output = value;
+
+    } );
+    gui.add( saoPass.params, 'saoBias', - 1, 1 );
+    gui.add( saoPass.params, 'saoIntensity', 0, 1 );
+    gui.add( saoPass.params, 'saoScale', 0, 10 );
+    gui.add( saoPass.params, 'saoKernelRadius', 1, 100 );
+    gui.add( saoPass.params, 'saoMinResolution', 0, 1 );
+    gui.add( saoPass.params, 'saoBlur' );
+    gui.add( saoPass.params, 'saoBlurRadius', 0, 200 );
+    gui.add( saoPass.params, 'saoBlurStdDev', 0.5, 150 );
+    gui.add( saoPass.params, 'saoBlurDepthCutoff', 0.0, 0.1 );
+    gui.add( saoPass, 'enabled' );*/
 
 /// test-geometry ///
 
@@ -109,8 +162,18 @@ function onWindowResize(): void {
     render();
 }
 
+function animate() {
+    requestAnimationFrame( animate );
+
+    stats.begin();
+    render();
+    stats.end();
+}
+
 function render(): void {
-    renderer.render(scene, camera);
+    saoPass.enabled = !isDragging;
+    composer.render();
 }
 
 init();
+animate();
